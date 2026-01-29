fb76433208d34e770d66b52bc617a7379bccc26c
Emily Filanowski <emily.filanowski@uconn.edu>
2026-01-29 11:52:49 -0500

    in class work

---
+++ week-02/script.js
@@ -0,0 +1,28 @@
+var week = 2;
+
+console.log("Hello, this is week " + week + ".");
+
+//alert("hello, this is week 2")
+
+var today = "Thursday";
+//console.log(today);
+var tomorrow = "Friday";
+
+//console.log("Today is Thursday");
+//console.log("Tomorrow is Friday");
+
+var greeting = "Today is " + today + ".";
+
+console.log(greeting);
+
+var nextWeek = week + 1;
+
+console.log("Next week is " + nextWeek + ".");
+
+//var myName = "Emily";
+
+
+var myName = prompt("What is your name?");
+
+alert("My name is " + myName);
+
